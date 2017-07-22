f6fac01da99b897d2753c11e687283b59c1322dd
Ashwin Karichannavar <ashwinkk.23@gmail.com>
2017-07-22 15:10:45 +0530

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # result_analysis
 College Semester Result analysis software using pandas. Generates a PDF report of the data(data visualization).
+
+Requirements:
+    Python 3.x
+        modules:
+            Numpy
+            Scipy
+            csv 
+            matplotlib
+            seaborn
+            Tkinter
+            tkMessageBox
+            pandas
+            
+User Inputs:
+    Path of the datafile (type: textbox)
+    Department (type: radio button)
+    Name for the pdf report or the path(type: textbox)
+    
+Output:
+    PDF File
+    
+To freeze the code on windows:
+    execute "py2exe setup.py" from cmd.
+    paste "data.ico" in dist folder.
--- results_data_analysis_GUI.py
+++ results_data_analysis_GUI.py
@@ -105,8 +105,6 @@ def main():
         elif int(radio.get()) == 5:
             dep = 'CV'
         else:
-            #dep = "Enter department"
-            #print(dep)
             tkMessageBox.showinfo("Error!","Choose Valid Department")
 
         #Extracting required information, type = list
@@ -143,122 +141,112 @@ def main():
         dfec41=pd.DataFrame.from_records(ec41,columns=lable1)
         lable2 = ['Result','Number']
         tot_res = pd.DataFrame.from_records(tot,columns=lable2)
-
-        #print("Analysis completed!")
-        #print(e2.get())
-        #print(dep)
         
                 
-        #print("MAT"+dep+"41")
         fig1 = plt.figure(1)
         sns.barplot(x="Grade", y="Number", data=dfec41,palette="Reds_d")
         fig1.suptitle("MAT"+dep+"41",fontsize = 14,fontweight ='bold')
-        #fig1.show()
 
         fig2 = plt.figure(2)
         result = dfec41.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig2.suptitle("MAT"+dep+"41_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig2.show()
 
-        #print(dep+"42")
+        
         fig3 = plt.figure(3)
         sns.barplot(x="Grade", y="Number", data=dfec42,palette="Greens_d")
         fig3.suptitle(dep+"42",fontsize = 14,fontweight ='bold')
-        #fig3.show()
+        
 
         fig4 = plt.figure(4)
         result = dfec42.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig4.suptitle(dep+"42_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig4.show()
-
-        #print(dep+"43")
+        
         fig5 = plt.figure(5)
         sns.barplot(x="Grade", y="Number", data=dfec43,palette="Blues_d")
         fig5.suptitle(dep+"43",fontsize = 14,fontweight ='bold')
-        #fig5.show()
+        
 
         fig6 = plt.figure(6)
         result = dfec43.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig6.suptitle(dep+"43_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig6.show()
+        
 
-        #print(dep+"44")
+        
         fig7 = plt.figure(7)
         sns.barplot(x="Grade", y="Number", data=dfec44,palette="Purples_d")
         fig7.suptitle(dep+"44",fontsize = 14,fontweight ='bold')
-        #fig7.show()
+        
 
         fig8 = plt.figure(8)
         result = dfec44.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig8.suptitle(dep+"44_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig8.show()
+        
 
-        #print(dep+"45")
+        
         fig9 = plt.figure(9)
         sns.barplot(x="Grade", y="Number", data=dfec45,palette="BuGn_r")
         fig9.suptitle(dep+"45",fontsize = 14,fontweight ='bold')
-        #fig9.show()
+        
 
         fig10 = plt.figure(10)
         result = dfec45.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig10.suptitle(dep+"45_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig10.show()
+        
 
-        #print(dep+"46")
+        
         fig11 = plt.figure(11)
         sns.barplot(x="Grade", y="Number", data=dfec46,palette="GnBu_d")
         fig11.suptitle(dep+"46",fontsize = 14,fontweight ='bold')
-        #fig11.show()
+        
 
         fig12 = plt.figure(12)
         result = dfec46.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig12.suptitle(dep+"46_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig12.show()
+        
 
-        #print(dep+"L47")
+        
         fig13 = plt.figure(13)
         sns.barplot(x="Grade", y="Number", data=dfec47,palette="RdBu_r")
         fig13.suptitle(dep+"L47",fontsize = 14,fontweight ='bold')
-        #fig13.show()
+        
 
         fig14 = plt.figure(14)
         result = dfec47.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig14.suptitle(dep+"L47_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig14.show()
+        
 
-        #print(dep+"L48")
+        
         fig15 = plt.figure(15)
         sns.barplot(x="Grade", y="Number", data=dfec48,palette="YlGn")
         fig15.suptitle(dep+"L48",fontsize = 14,fontweight ='bold')
-        #fig15.show()
+        
 
         fig16 = plt.figure(16)
         result = dfec48.pivot(index='Grade',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig16.suptitle(dep+"L48_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig16.show()
+        
 
-        #print("Overall Result")
+        #Overall Result
         fig17 = plt.figure(17)
         sns.barplot(x='Result',y='Number',data=tot_res,palette="OrRd")
         fig17.suptitle("Overall Result",fontsize = 14,fontweight ='bold')
-        #fig17.show()
+        
 
         fig18 = plt.figure(18)
         result = tot_res.pivot(index='Result',columns='Number',values='Number')
         sns.heatmap(result,square=True,annot=True)
         fig18.suptitle("OverAll_Result_HeatMap",fontsize = 14,fontweight ='bold')
-        #fig18.show()
+        
 
         #Saving Result to PDF file
-        #print ('Generating PDF Report\n')
         pp = PdfPages(str(e2.get())+'.pdf')
         pp.savefig(fig1)
         pp.savefig(fig2)
